e2c564c7435e9ed044ac32e65b82f4e66d2826e8
Wally Haven <wlhaven@gmail.com>
2018-05-24 10:12:00 -0700

    First commit to GitHub

---
+++ src/com/company/Lottery.java
@@ -0,0 +1,120 @@
+package com.company;
+/*
+  Created by Wally Haven on 5/16/2018.
+ */
+
+import java.io.*;
+import java.util.InputMismatchException;
+import java.util.Random;
+import java.util.Arrays;
+import java.util.Scanner;
+
+import static java.lang.String.format;
+
+
+class Lottery {
+    private final int SIZE;
+    private final int MEGABUCKS;
+    private final int[] lotto_ticket;
+    private int num_boards;
+    private String answer;
+    private final String outFileName;
+    private FileWriter outFile;
+    private Scanner scanner;
+    private final char myCopyRight;
+
+
+    Lottery() {
+        SIZE = 6;
+        MEGABUCKS = 48;
+        num_boards = 0;
+        answer = "n";
+        outFileName = "LotteryNumbers.txt";
+        outFile = null;
+        scanner = new Scanner(System.in);
+        lotto_ticket = new int[SIZE];
+        myCopyRight = '\u00A9';  //unicode copyright symbol
+    }
+
+    void generateNumbers() throws IOException {
+        while (!answer.equals("Y")) {
+            System.out.println("\n\t\t JAVA LOTTERY NUMBER GENERATOR");
+            System.out.println("\t\t\t" + myCopyRight + java.time.Year.now() + " by Wally Haven");
+            System.out.println("\nHow many sets of numbers do you wish to play?");
+            num_boards = getUserNumber();
+            Random rand = new Random();
+            createFile();
+
+            for (var index = 1; index <= num_boards; index++) {
+                System.out.println(format("\nBoard: %d", index));
+                outFile.write(String.format("\n" + "Board: %d \n", index));
+
+                for (var i = 0; i < SIZE; i++) {
+                    int number = rand.nextInt(MEGABUCKS) + 1;
+                    lotto_ticket[i] = number;
+                    var tmp_num = lotto_ticket[i];
+                    var flag = searchTicket(lotto_ticket, tmp_num, i);
+                    while (flag) {
+                        tmp_num = lotto_ticket[i];
+                        flag = searchTicket(lotto_ticket, tmp_num, i);
+                        if (flag) {
+                            lotto_ticket[i] = rand.nextInt(MEGABUCKS) + 1;
+                        }
+                    }
+                }
+                Arrays.sort(lotto_ticket);
+
+                for (var ticket : lotto_ticket) {
+                    try {
+                        System.out.println(ticket + " ");
+                        outFile.write(String.valueOf(ticket) + " ");
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            System.out.println("\nDo you wish to exit?");
+            answer = getUserString();
+        }
+        outFile.close();
+    }
+
+    private void createFile() {
+        try {
+            outFile = new FileWriter(outFileName);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private int getUserNumber() {
+        for (; ; ) {
+            Scanner scanner = new Scanner(System.in);
+            try {
+                return scanner.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("Input was not a number. Please retry.");
+            }
+        }
+    }
+
+    private String getUserString() {
+        String inputLine;
+        scanner = new Scanner(System.in);
+
+        inputLine = scanner.nextLine();
+        return inputLine.toUpperCase();
+    }
+
+    private boolean searchTicket(int lotto_ticket[], int num, int size) {
+        var count = 0;
+        while (count < size) {
+            if (num == lotto_ticket[count]) {
+                return (true);
+            }
+            count++;
+        }
+        return (false);
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import java.io.IOException;
+
+class Main {
+
+    public static void main(String[] args) throws IOException {
+        Lottery LotteryNumber = new Lottery();
+        LotteryNumber.generateNumbers();
+    }
+}
